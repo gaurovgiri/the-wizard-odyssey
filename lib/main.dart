457e63322f6bbce05d806b50713c490c3cd6c282
gaurovgiri <gaurovgiri@gmail.com>
2024-01-13 10:25:42 +0545

    add wizard sprite and parallax

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:runner/game.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const Runner());
+}
+
+class Runner extends StatelessWidget {
+  const Runner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: RunnerHome());
+  }
+}
+
+// Just for running the our game widget instance
+class RunnerHome extends StatefulWidget {
+  const RunnerHome({super.key});
+
+  @override
+  State<RunnerHome> createState() => _RunnerHomeState();
+}
+
+class _RunnerHomeState extends State<RunnerHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: GameWidget(game: RunnerGame()));
+  }
+}
